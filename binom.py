84c30716d0e2c6f6ee98d4ae9b2f695ebee2f2be
terjebh <terje@itfakultetet.no>
2022-02-10 17:18:22 +0100

    range

--- binom.py
+++ binom.py
@@ -1,7 +1,15 @@
 import numpy as np 
-import scipy as sc
-import sklearn as sk
+from scipy import stats
+from matplotlib import pyplot as pl
+
+prob_flere = stats.binom_test(15,20,0.5)
+prob_en = stats.binom.pmf(range(20),20,0.5)
+
+print(prob_flere)
+
+print(prob_en)
 
 
 
-
+pl.scatter(range(20),prob_en)
+pl.show()
